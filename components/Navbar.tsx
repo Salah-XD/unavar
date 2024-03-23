90457c72a23601900723a6c2c941d253223f9627
Salah-XD <hudeensala948@gmail.com>
2024-03-23 20:20:18 +0530

    bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className='bg-white p-7 shadow-lg flex items-center justify-between'>
+    <div className="bg-white p-7 shadow-lg flex items-center justify-between">
       <div className="left flex items-center gap-5">
-        <Image
-          src= "/img/image 3.png"
-          width={50}
-          height={50}
-        />
-        <p className='font-bold'>UNAVAR FOOD INSPECTION & <br /> CERTIFICATION PRIVATE LIMITED</p>
- 
+        <Image alt="logo" src="/img/image 3.png" width={50} height={50} />
+        <p className="font-bold">
+          UNAVAR FOOD INSPECTION & <br /> CERTIFICATION PRIVATE LIMITED
+        </p>
       </div>
       <div className="right">
         <div className="menu flex gap-7 list-none font-[500]">
-          <li><a href="">Home</a></li>
-          <li><a href="">About Us</a></li>
-          <li><a href="">Services</a></li>
-          <li><a href="">Industry</a></li>
-          <li><a href="">clients</a></li>
-          <li><a href="">Blogs</a></li>
-          <li><a href="">Contact Us</a></li>
-        </div> 
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">Industry</a>
+          </li>
+          <li>
+            <a href="">clients</a>
+          </li>
+          <li>
+            <a href="">Blogs</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
