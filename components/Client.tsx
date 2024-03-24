2d2fa701caa68066cb920b6d3d088656a12c0b94
HARISH <112271304+HARISH-DEV433@users.noreply.github.com>
2024-03-24 20:08:56 +0530

    props_added

--- components/Client.tsx
+++ components/Client.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 
+const img1 = "/images/Sapphire.png";
+const img2 = "/images/Anjappar.jpg";
+const img3 = "/images/HardCastle.png";
+const img4 = "/images/Jubilant.png";
+const img5 = "/images/Kitchen.jpg"
+const img6 = "/images/Kitchen.jpg"
+const img7 = "/images/Kitchen.jpg"
+
 const Clients = () => {
     return (
         <div className='h-[100vh] bg-white py-10'>
@@ -12,27 +20,27 @@ const Clients = () => {
                 <div className='flex flex-col gap-3'>
                 <div className='flex py-2 justify-center gap-10'>
                     <div className='bg-white  w-[15vw] h-[30vh] flex justify-center align-middle rounded-2xl shadow-lg'>
-                        <Image alt="img" src="/images/Sapphire.png" width={200} height={50} className='object-cover rounded-2xl'/>
+                        <img src={img1} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                        <Image alt="img" src="/images/Anjappar.jpg" width={200} height={50} className='object-cover rounded-2xl' />
+                        <img src={img2} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                        <Image alt="img" src="/images/HardCastle.png" width={200} height={50} className='object-cover rounded-2xl' />
+                        <img src={img3} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                        <Image alt="img" src="/images/Jubilant.png" width={200} height={50} className='object-cover rounded-2xl' />
+                        <img src={img4} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                 </div>
                 <div className='flex py-3 justify-center gap-10'>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                        <Image alt="img" src="/images/Kitchen.jpg" width={200} height={100} className='object-cover rounded-2xl' />
+                        <img src={img5} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                        <Image alt="img" src="/images/Kitchen.jpg" width={200} height={100} className='object-cover rounded-2xl' />
+                        <img src={img6} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                    <Image alt="img" src="/images/Kitchen.jpg" width={200} height={100} className='object-cover rounded-2xl' />
+                        <img src={img7} alt='img' className='object-cover rounded-2xl'></img>
                     </div>
                 </div>
                 </div>
