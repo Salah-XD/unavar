9d746f4b1a0d28744d36a8e88797137cef708d69
HARISH <112271304+HARISH-DEV433@users.noreply.github.com>
2024-03-24 15:57:16 +0530

    over.

--- components/Client.tsx
+++ components/Client.tsx
@@ -12,10 +12,10 @@ const Clients = () => {
                 <div className='flex flex-col gap-3'>
                 <div className='flex py-2 justify-center gap-10'>
                     <div className='bg-white  w-[15vw] h-[30vh] flex justify-center align-middle rounded-2xl shadow-lg'>
-                        <Image alt="img" src="/images/Sapphire.png" width={180} height={50} className='object-cover rounded-2xl'/>
+                        <Image alt="img" src="/images/Sapphire.png" width={200} height={50} className='object-cover rounded-2xl'/>
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
-                        <Image alt="img" src="/images/Anjappar.jpg" width={180} height={50} className='object-cover rounded-2xl' />
+                        <Image alt="img" src="/images/Anjappar.jpg" width={200} height={50} className='object-cover rounded-2xl' />
                     </div>
                     <div className='bg-white w-[15vw] h-[30vh] rounded-2xl shadow-lg flex justify-center align-middle'>
                         <Image alt="img" src="/images/HardCastle.png" width={200} height={50} className='object-cover rounded-2xl' />
