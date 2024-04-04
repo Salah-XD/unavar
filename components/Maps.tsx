efc58a908990e0d8ae1275ae9909e43d7a2fed14
HARISH <112271304+HARISH-DEV433@users.noreply.github.com>
2024-04-04 11:28:13 +0530

    Changes

--- components/Maps.tsx
+++ components/Maps.tsx
@@ -4,22 +4,22 @@ type Props = {}
 
 const Maps = (props: Props) => {
   return (
-    <div className="relative overflow-hidden bg-cover bg-no-repeat p-40 text-center bg-img bg-[url(/images/Maps.png)]">
+    <div className="relative inset-0 overflow-hidden bg-cover bg-no-repeat p-40 text-center bg-img bg-[url(/images/Maps.png)] bg-center filter backdrop-blur-3xl ">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <a className="text-4xl font-bold text-white">All Across India</a>
-          <div>
-            <ul>
-                <li>NEW DELHI</li>
-                <li>MUMBAI</li>
-                <li>BANGLORE</li>
-                <li>CHENNAI</li>
-                <li>HYDERABAD</li>
+          <div className='flex text-white flex-col gap-5 p-[30px]'>
+            <ul className='flex gap-8 justify-center'>
+                <li className='text-white font-bold'>NEW DELHI</li>
+                <li className='text-white font-bold'>MUMBAI</li>
+                <li className='text-white font-bold'>BANGLORE</li>
+                <li className='text-white font-bold'>CHENNAI</li>
+                <li className='text-white font-bold'>HYDERABAD</li>
             </ul>
-            <ul>
-                <li>AHMEDABAD</li>
-                <li>COCHIN</li>
-                <li>KOLKATA</li>
-                <li>INDORE</li>
+            <ul className='flex gap-8 justify-center'>
+                <li className='text-white font-bold'>AHMEDABAD</li>
+                <li className='text-white font-bold'>COCHIN</li>
+                <li className='text-white font-bold'>KOLKATA</li>
+                <li className='text-white font-bold'>INDORE</li>
             </ul>
             </div>
         </div>
