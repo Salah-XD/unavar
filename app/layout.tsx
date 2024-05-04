07a4f0c829f9d4a39b725b1b4c98a22058b7f0b1
Salah-XD <hudeensala948@gmail.com>
2024-05-04 10:15:02 +0530

    ch:font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 // Import necessary dependencies
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google"; // Import the Poppins font
-import { Roboto_Slab } from "next/font/google"; // Import the Roboto_Slab font (Fix typo here)
+// import { Roboto_Slab } from "next/font/google"; // Import the Roboto_Slab font (Fix typo here)
 import "./globals.css"; // Import global styles
 
 // Configure the Poppins font
@@ -15,10 +15,10 @@ const poppins = Poppins({
 });
 
 // Configure the RobotoSlab font (Fix typo in the function name)
-export const RobotoSlab = Roboto_Slab({
-  subsets: ["latin"],
-  display: "swap",
-});
+// export const RobotoSlab = Roboto_Slab({
+//   subsets: ["latin"],
+//   display: "swap",
+// });
 
 // Define metadata for the application
 export const metadata: Metadata = {
